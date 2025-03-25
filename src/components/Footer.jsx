2473df697fc9807b0509b43e699c553a480ea302
Nachzzz <nachosalto17@gmail.com>
2025-03-25 15:23:43 -0300

    Ultimos cambios

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,11 +4,11 @@ import './Footer.css';
 const Footer = () => {
     return (
         <footer className="footer">
-            <p>© {new Date().getFullYear()} Tiempito - Todos los derechos reservados</p>
+            <p>© {new Date().getFullYear()} Tiempito </p>
             <div className="footer-links">
-                <a href="nachosalto17@gmail.com" className="footer-link">📧 Contacto</a>
-                <a href="https://twitter.com/tuusuario" target="_blank" rel="noopener noreferrer" className="footer-link">🐦 Twitter</a>
-                <a href="https://www.instagram.com/tuusuario" target="_blank" rel="noopener noreferrer" className="footer-link">📸 Instagram</a>
+                <a href="mailto:nachosalto17@gmail.com" className="footer-link">📧 Contacto</a>
+                <a href="https://www.linkedin.com/in/jorge-ignacio-salto-0b29221bb" target="_blank" rel="noopener noreferrer" className="footer-link">💻 LinkedIn</a>
+                <a href="https://github.com/Nachzzz" target="_blank" rel="noopener noreferrer" className="footer-link">👨‍💻 GitHub</a>
             </div>
         </footer>
     );
