39a86e05c3d9635c1c6b859b7914e2c439d3c590
Nachzzz <nachosalto17@gmail.com>
2025-03-25 15:13:14 -0300

    agregando

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,15 @@
-import React from "react";
-import "./Footer.css";
+import React from 'react';
+import './Footer.css';
 
 const Footer = () => {
     return (
         <footer className="footer">
-            <p>© {new Date().getFullYear()} Tiempito - Todos los derechos reservados.</p>
+            <p>© {new Date().getFullYear()} Tiempito - Todos los derechos reservados</p>
+            <div className="footer-links">
+                <a href="nachosalto17@gmail.com" className="footer-link">📧 Contacto</a>
+                <a href="https://twitter.com/tuusuario" target="_blank" rel="noopener noreferrer" className="footer-link">🐦 Twitter</a>
+                <a href="https://www.instagram.com/tuusuario" target="_blank" rel="noopener noreferrer" className="footer-link">📸 Instagram</a>
+            </div>
         </footer>
     );
 };
